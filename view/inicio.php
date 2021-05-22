aa670889a10e37da6e07f5930d81493c88f7c2df
victor17-code <2016110312@udh.edu.pe>
2021-05-22 18:28:30 -0500

    prueba

---
+++ view/inicio.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=drivice-width, user-scalable=no, initial-scalable=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        <title>prueba</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+    <body>  
+        
+        <?php
+        echo 'hola mundo';
+        ?>
+        <script src="js/jquery-3.6.0.min.js" ></script>
+        <script src="js/popper.min.js" ></script>
+        <script src="js/bootstrap.min.js" ></script>       
+    </body>
+</html>
