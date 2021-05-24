8293280d7a2dc19c9bcdeb1d1f235141eaf4f1f8
victor17-code <2016110312@udh.edu.pe>
2021-05-23 22:32:39 -0500

    avance

--- view/inicio.php
+++ view/inicio.php
@@ -1,18 +1,79 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=drivice-width, user-scalable=no, initial-scalable=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-        <title>prueba</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <title>Buscando Servicio</title>
     </head>
-    <body>  
-        
-        <?php
-        echo 'hola mundo';
-        ?>
-        <script src="js/jquery-3.6.0.min.js" ></script>
-        <script src="js/popper.min.js" ></script>
-        <script src="js/bootstrap.min.js" ></script>       
+    <body>
+        <!--inicio de nav bar-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-info">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="../img/logo.png" class="img-fluid" width="100px" height="100px"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                        </li>                       
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Categorias                               
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Mecanica</a></li>
+                                <li><a class="dropdown-item" href="#">todo</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">nada</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Sobre Nosotros</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Buscar Servicio" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    <button type="button" class="btn btn-success">Iniciar Sesión</button>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>¿No sabes Donde Buscar el Producto o Servicio que desees en tu localidad?</h2>
+                    <h2><center>Estas en el lugar correcto</center></h2>
+                </div>
+                <div class="col-12">
+                    <img src="../img/mecanica.jpg">
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h4>Recientes</h4>  
+                </div>
+                <div class="col-4">
+                    Primera de tres columnas
+                </div>
+                <div class="col-4">
+                    Segunda de tres columnas
+                </div>
+                <div class="col-4">
+                    Tercera de tres columnas
+                </div>
+            </div>
+        </div>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
     </body>
 </html>
