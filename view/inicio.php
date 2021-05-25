3c219b96cc921d8f1ba6844dff5c0248a8f14e1d
abelmartin10 <abel.martin10@outlook.com>
2021-05-24 22:29:54 -0500

    editado 1

--- view/inicio.php
+++ view/inicio.php
@@ -7,7 +7,7 @@ include './header.php';
         <div class="row">
             <div class="col-12">
                 <center><h2>¿No sabes Donde Buscar el Producto o Servicio que desees en tu localidad?</h2>
-                    <h2>Estas en el lugar correcto</h2></center>
+                    <h2>Estas en el lugar correcto prueba1</h2></center>
             </div>
             <div class="col-12">
                 <img src="../img/mecanica.jpg">
