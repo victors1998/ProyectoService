c4c7b6f1efe4ebb4c2d65d8fbb0d2c5aefc98968
victor17-code <2016110312@udh.edu.pe>
2021-05-28 00:33:22 -0500

    login

--- bd/proyecto_service.sql
+++ bd/proyecto_service.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-05-2021 a las 05:30:35
+-- Tiempo de generación: 28-05-2021 a las 07:12:53
 -- Versión del servidor: 10.4.18-MariaDB
 -- Versión de PHP: 8.0.3
 
@@ -38,8 +38,12 @@ CREATE TABLE `categoria` (
 --
 
 INSERT INTO `categoria` (`idcategoria`, `nombre_categoria`, `estado`) VALUES
-(0, 'Lacteos', '1'),
-(1, 'Calzados', '1');
+(0, 'Tiendas', '1'),
+(1, 'Tiendas', '1'),
+(2, 'Calzados', '1'),
+(3, 'Mecanicas', '1'),
+(4, 'Lavaderos', '1'),
+(5, 'Casas de apuestas', '1');
 
 -- --------------------------------------------------------
 
@@ -54,6 +58,32 @@ CREATE TABLE `imagen` (
   `url` varchar(100) DEFAULT NULL,
   `imagencol` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `negocio`
+--
+
+CREATE TABLE `negocio` (
+  `idnegocio` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `ruc` varchar(45) NOT NULL,
+  `direccion` varchar(45) NOT NULL,
+  `estado` varchar(45) NOT NULL,
+  `usuario_idUsuario` int(11) NOT NULL,
+  `fecha_registro` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `negocio`
+--
+
+INSERT INTO `negocio` (`idnegocio`, `nombre`, `ruc`, `direccion`, `estado`, `usuario_idUsuario`, `fecha_registro`) VALUES
+(1, 'Canzados Viki', '66448099', 'Av. República de Panamá 4050 Urb. Limatambo, ', '1', 1, '2021-05-27'),
+(2, 'Te apuesto', '58696', 'Av. República de Panamá 4050 Urb. Limatambo, ', '1', 1, '2021-05-24'),
+(3, 'Te apuesto', '58696', 'Av. República de Panamá 4050 Urb. LimatamboSu', '1', 1, '2021-05-24'),
+(4, 'Abarrotes Fany', '66448099', 'av san peres', '1', 1, '2021-05-24');
 
 -- --------------------------------------------------------
 
@@ -80,19 +110,19 @@ CREATE TABLE `servicio` (
   `titulo` varchar(45) NOT NULL,
   `descripcion` varchar(45) NOT NULL,
   `estado` varchar(45) NOT NULL,
-  `id_usuario` int(11) NOT NULL,
   `fecha_registro` date NOT NULL,
-  `id_categoria` int(11) NOT NULL
+  `id_categoria` int(11) NOT NULL,
+  `negocio_idnegocio` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `servicio`
 --
 
-INSERT INTO `servicio` (`idServicio`, `titulo`, `descripcion`, `estado`, `id_usuario`, `fecha_registro`, `id_categoria`) VALUES
-(2, 'Zapateria & Lenceria', 'Todo tipo de zapatos de Moda', '1', 1, '2021-05-24', 1),
-(3, 'Peluqueria shantal', 'sgethrety', '1', 1, '2021-05-24', 0),
-(4, 'Restaurant Lopez', 'Comidas Tipicas', '1', 1, '2021-05-24', 1);
+INSERT INTO `servicio` (`idServicio`, `titulo`, `descripcion`, `estado`, `fecha_registro`, `id_categoria`, `negocio_idnegocio`) VALUES
+(1, 'Venta de zapatos', 'Todo tipo de zapatos de Moda', '1', '2021-05-27', 2, 1),
+(2, 'Venta de zapatos', 'Todo tipo de zapatos de Moda', '1', '2021-05-27', 2, 1),
+(3, 'Restaurant Lopez', 'Todo tipo de zapatos de Moda', '1', '2021-05-24', 4, 3);
 
 -- --------------------------------------------------------
 
@@ -118,7 +148,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idUsuario`, `nombres`, `apellidos`, `tipoUsuario`, `userName`, `password`, `estado`, `dni`, `telefono`, `fecha_registro`) VALUES
-(1, 'Victor', 'eusebio soto', 'admin', 'victor17', 'admin', '1', '73515797', '987545689', '2021-05-24');
+(1, 'Victor', 'eusebio soto', 'admin', 'victor17', '1234', '1', '73515797', '987545689', '2021-05-27');
 
 --
 -- Índices para tablas volcadas
@@ -138,6 +168,13 @@ ALTER TABLE `imagen`
   ADD KEY `fk_imagen_servicio1_idx` (`id_servicio`);
 
 --
+-- Indices de la tabla `negocio`
+--
+ALTER TABLE `negocio`
+  ADD PRIMARY KEY (`idnegocio`),
+  ADD KEY `fk_negocio_usuario1_idx` (`usuario_idUsuario`);
+
+--
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
@@ -149,8 +186,8 @@ ALTER TABLE `productos`
 --
 ALTER TABLE `servicio`
   ADD PRIMARY KEY (`idServicio`),
-  ADD KEY `fk_servicio_usuario_idx` (`id_usuario`),
-  ADD KEY `fk_servicio_categoria1_idx` (`id_categoria`);
+  ADD KEY `fk_servicio_categoria1_idx` (`id_categoria`),
+  ADD KEY `fk_servicio_negocio1_idx` (`negocio_idnegocio`);
 
 --
 -- Indices de la tabla `usuario`
@@ -163,6 +200,12 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `negocio`
+--
+ALTER TABLE `negocio`
+  MODIFY `idnegocio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
@@ -172,7 +215,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `servicio`
 --
 ALTER TABLE `servicio`
-  MODIFY `idServicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idServicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
@@ -191,6 +234,12 @@ ALTER TABLE `imagen`
   ADD CONSTRAINT `fk_imagen_servicio1` FOREIGN KEY (`id_servicio`) REFERENCES `servicio` (`idServicio`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
+-- Filtros para la tabla `negocio`
+--
+ALTER TABLE `negocio`
+  ADD CONSTRAINT `fk_negocio_usuario1` FOREIGN KEY (`usuario_idUsuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
 -- Filtros para la tabla `productos`
 --
 ALTER TABLE `productos`
@@ -201,7 +250,7 @@ ALTER TABLE `productos`
 --
 ALTER TABLE `servicio`
   ADD CONSTRAINT `fk_servicio_categoria1` FOREIGN KEY (`id_categoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_servicio_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_servicio_negocio1` FOREIGN KEY (`negocio_idnegocio`) REFERENCES `negocio` (`idnegocio`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
