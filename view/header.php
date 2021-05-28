f84f4f9c40498ff02b66ee8dd734fb1948708b3c
victor17-code <2016110312@udh.edu.pe>
2021-05-28 17:39:19 -0500

    btn

--- view/header.php
+++ view/header.php
@@ -54,6 +54,20 @@
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
                     <button type="button"  class="btn btn-success"><a href="login.php">Iniciar Sesión</a></button>
+                    <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+<!--                        <button type="button" class="btn btn-primary">1</button>
+                        <button type="button" class="btn btn-primary">2</button>-->
+
+                        <div class="btn-group" role="group">
+                            <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
+                                <li><a class="dropdown-item" href="#">Dropdown link</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
